7a29f47d6c7de33c57de863a4625f9303db72f7d
Lanna-27 <lalacast2000@gmail.com>
2024-03-16 17:29:44 -0500

    Add rabbitMQ tutorial

--- swarch2024i/swarch2024i_ms/swarch2024i_ms/models/category_model.py
+++ swarch2024i/swarch2024i_ms/swarch2024i_ms/models/category_model.py
@@ -3,8 +3,8 @@ from django.db import models
 class Category(models.Model):
 
     id = models.AutoField(primary_key = True)
-    name = models.CharField(max_length = 20)
-    description = models.TextField(max_length = 140)
+    name = models.CharField(max_length = 50)
+    description = models.TextField(max_length = 200)
 
     class Meta:
         app_label = 'swarch2024i_ms'
